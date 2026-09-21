d18c062a9de54d365945de58c97b5dde2135d9ee
Shashank Tiwari <shashank.mgm@outlook.com>
2026-09-21 12:12:21 +0530

    Fix Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { images: { layoutRaw: true } },
 };
-const withImages = require("next-images");
-module.exports = withImages();
+
 module.exports = nextConfig;
+
